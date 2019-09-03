1b07127694e64e0a983c78afb06d7d56911c8d29
Noonenew <30725615+noonenew@users.noreply.github.com>
2019-09-03 12:51:49 +0300

    Update map-template.blade.php

--- src/resources/views/map-template.blade.php
+++ src/resources/views/map-template.blade.php
@@ -1,43 +1,31 @@
-<canvas id="{!! $element !!}" width="{!! $size['width'] !!}" height="{!! $size['height'] !!}">
+<div id="{!! $element !!}" style="width: auto; height: {!! $size['height'] !!}px; position: relative; overflow: hidden; background-color: transparent;">
+
+</div>
 <script type="application/javascript">
     document.addEventListener("DOMContentLoaded", function(event) {
         (function() {
-    		"use strict";
-            			let map = L.map('{!! $mapid !!}', {scrollWheelZoom: {!! $scrollWheelZoom !!},maxZoom: {!! $maxZoom !!} }).setView([{!! $mapLat !!},{!! $mapLong !!}],{!! $zoom !!});
-			let controlCustom = new L.Control.Custom(null,null,{ collapsed: true }).addTo(map);
-			let legendCustom = new L.Control.Customs(null,null,{ collapsed: true }).addTo(map);
-			let stateChangingButton = L.easyButton({
-				states: [{
-					stateName: 'zoom-to-home',        // name the state
-					icon:      'fa-home',               // and define its properties
-					title:     'zoom to a home',      // like its title
-					onClick: function(btn, map) {       // and its callback
-					     map.setView([{!! $mapLat !!}, {!! $mapLong !!}],{!! $zoom !!});
-						 }
-						 }]
-						 });
-					stateChangingButton.addTo(map);
-					map.zoomControl.setPosition('topleft');
-					map.addControl(new L.Control.Fullscreen().setPosition('topleft'));
-					stateChangingButton.setPosition('topleft');
-					// informationicon.setPosition('bottomright');
-					legend.setPosition('bottomleft');
-					
-					let osm2 = new L.TileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {minZoom: 0, maxZoom: 8, attribution: 'Map data &copy; OpenStreetMap contributors'});
-					let miniMap = new L.Control.MiniMap(osm2,{toggleDisplay: true, minimized: true }).setPosition('bottomright').addTo(map);
-					
-					let basemap = L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
-						attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-						}).addTo(map);
-						
-					let markers = L.markerClusterGroup({
-						spiderfyOnMaxZoom: true,
-						showCoverageOnHover: false,
-						zoomToBoundsOnClick: true
-						});
-						var datachange = [];
-						markers.addTo(map);
+            "use strict";
+            let map = L.map('{!! $element !!}', {scrollWheelZoom: false,maxZoom: 18}).setView([15.387664,47.190976],6.2);
+            let basemap = L.tileLayer('{!! $type["maplink"] !!}', {
+                attribution: '{!! $type["attribution"] !!}'
+            }).addTo(map);
+	    
+            let datasets = JSON.parse({!! $datasets !!});
+	    
+            datasets.forEach(function (datas) {
+            let tooltips = "<br/>" +
+                "<br/>";
+
+            let popup = "<h4>Spot-light</h4><br/>" +
+                "<lable><b>Company Name :</b> "+datas.latitude+".</lable><br/>" +
+                "<lable><b>Beneficiary Name in Arabic :</b> "+datas.longitude+".</lable><br/>" +
+                "<br/>";
+
+                var m = L.marker([datas.latitude, datas.longitude]).bindPopup(popup).bindTooltip(tooltips,
+                {className: "leaflet-label-marker", offset: [0, 0] }).addTo(map);
+
+            });
+	    
         })();
     });
 </script>
-</canvas>
