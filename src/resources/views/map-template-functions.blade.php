c54ad6c6f4f9b976f1a77d4f30ce4f3258754b13
Mohammed Fawzi <30725615+noonenew@users.noreply.github.com>
2019-09-28 11:35:43 +0300

    Update map-template-functions.blade.php

--- src/resources/views/map-template-functions.blade.php
+++ src/resources/views/map-template-functions.blade.php
@@ -1,10 +1,5 @@
 <script id="map-function" type="application/javascript">
 
-function processMarkers(mapdata) {
-    mapdata.forEach(function (data) {
-        var m = L.marker([data.latitude, data.longitude], {title: title,icon: myIcon});
-    });
-}
-
+    {!! $customfunction !!}
 
 </script>
